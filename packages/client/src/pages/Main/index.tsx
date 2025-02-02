3ad88c351dce3cc398cbb998a847050493e1bcec
Ilya <iosdsbb@gmail.com>
2025-02-02 18:20:57 +0300

    feat: added MainPage

---
+++ packages/client/src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <div className="main">
+      <h1>Main page</h1>
+      <p>Hello, Monorepo!</p>
+    </div>
+  )
+}
+
+export default MainPage;
